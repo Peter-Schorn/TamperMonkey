7dc45b245b415b706ab5eb598f7bab99b1ac30f7
Peter Schorn <petervschorn@gmail.com>
2025-11-16 15:41:55 -0600

    stop tracking tsconfig.tsbuildinfo

--- src/types/chatGPT-prompt-history.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace ChatGPTTypes {
-    interface Window extends globalThis.Window {
-        getCurrentPromptText(): string;
-        getUserMessages(): string[];
-        updateUserMessages(): void;
-    }
-}
--- src/types/reconnecting-websocket-types.d.ts
+++
@@ -1,7 +0,0 @@
-import * as _ReconnectingWebSocket from "reconnectingwebsocket";
-
-declare global {
-    export import ReconnectingWebSocket = _ReconnectingWebSocket;
-}
-
-export { };
